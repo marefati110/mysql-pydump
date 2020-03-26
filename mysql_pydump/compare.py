4c00617a19e50a4bc05b56ee850d25312482c569
ali <marefati110@gmail.com>
2020-03-27 01:52:13 +0430

    added compare function but not used

--- mysql_pydump/compare.py
+++ mysql_pydump/compare.py
@@ -1,18 +1,50 @@
-# # from .dump import sql_dump
-#
-# differ = Differ()
-#
-# a = """ ali
-#         amir
-#         reza """.splitlines(keepends=True)
-#
-# b = """ ali
-#         reza
-#         amir """.splitlines(keepends=True)
-#
-# # res = list(differ.compare(a, b))
-# # print(res)
-# #
-# # print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__, __name__, str(__package__)))
-# #
-# # count = 5
+def remove_common(result: list) -> list:
+    output: list = []
+    for elm in result:
+        if '+' in elm[:2] or '-' in elm[:2]:
+            output.append(elm)
+        else:
+            pass
+    result.clear()
+    return output
+
+
+def fix_diff(result: list) -> list:
+    output: list = []
+
+    for elm in result:
+        if elm[0] == '+':
+            check: bool = '-' + elm[1:] in result
+            if check:
+                continue
+            else:
+                output.append(elm)
+        elif elm[0] == '-':
+            check: bool = '+' + elm[1:] in result
+            if check:
+                continue
+            else:
+                output.append(elm)
+        else:
+            pass
+
+    result.clear()
+    return output
+
+
+def filter_by(result: list, remove_symbol: str) -> list:
+    output: list = []
+    for elm in result:
+        if remove_symbol in elm[:2]:
+            continue
+        else:
+            output.append(elm[2:])
+    result.clear()
+    return output
+
+
+def concat(result):
+    txt = ''
+    for elm in result:
+        txt += elm
+    return txt
